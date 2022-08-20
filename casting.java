6b191ac2c40396330449239ca17d6cfcbffcf470
Satyam <satyamsobhraj819@gmail.com>
2022-08-20 23:42:27 +0530

    first commit

---
+++ casting.java
@@ -0,0 +1,15 @@
+public class casting{
+    public static void main(String[] args) {
+        int a = 4;
+        double sum;
+        sum = a + 4.5;
+        System.out.println("sum: " +sum);
+        
+
+        int a1=42;
+        double Sum1;
+        Sum1= a1 + 44;
+        System.out.println("Sum1: " +Sum1);
+
+    }
+}
